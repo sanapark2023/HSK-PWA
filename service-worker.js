08bb9b564a47bb0f696e4a837cdb7550543b60a7
Ever-Sana <145433957+Ever-Sana@users.noreply.github.com>
2025-05-23 02:07:19 +0900

    버전업

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-      caches.open('hsk-quiz-v1').then(cache => {
+      caches.open('hsk-quiz-v2').then(cache => {
         return cache.addAll([
           './',
           './index.html',
